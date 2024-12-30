c597087de173a2444414f91a5840cd049441cdb4
EatGno <152440715+EatGno@users.noreply.github.com>
2024-12-30 12:26:44 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'paris rp'
+description 'Zombie Apocalypse Mode for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client/zombies.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+}
+
+shared_scripts {
+    'config.lua',
+}
